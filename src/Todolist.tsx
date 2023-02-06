ecc7252656ac2c3a92060d4eba326c6c22064e11
Andrey Paramonov <mikrobka231@gmail.com>
2023-02-06 18:46:06 +0300

    less 2

--- src/Todolist.tsx
+++ src/Todolist.tsx
@@ -1,38 +1,33 @@
-import React from "react";
+import React from 'react';
 
-type PropsType={
-    shapka?: string
-    newShapka?:string
-    tasks:Array<TaskType>
+type TaskType = {
+    id: number
+    title: string
+    isDone: boolean
 }
 
-type TaskType={
-    id:number
-    title:string
-    isDone:boolean
+type PropsType = {
+    title: string
+    tasks: Array<TaskType>
 }
 
-export const Todolist = (props:PropsType) => {
-    return (
-        <div className="App">
-            <div>
-                <h3>{props.shapka}</h3>
-                <h3>{props.newShapka}</h3>
-                <div>
-                    <input/>
-                    <button>+</button>
-                </div>
-                <ul>
-                    {props.tasks.map((el)=>{
-                        return <li key={el.id}><input type="checkbox" checked={el.isDone}/> <span>{el.title}</span></li>
-                    })}
-                </ul>
-                <div>
-                    <button>All</button>
-                    <button>Active</button>
-                    <button>Completed</button>
-                </div>
-            </div>
+export function Todolist(props: PropsType) {
+    return <div>
+        <h3>{props.title}</h3>
+        <div>
+            <input/>
+            <button>+</button>
         </div>
-    );
+        <ul>
+            {props.tasks.map((el) => {
+                return <li key={el.id}><input type="checkbox" checked={el.isDone}/> <span>{el.title}</span></li>
+            })}
+
+        </ul>
+        <div>
+            <button>All</button>
+            <button>Active</button>
+            <button>Completed</button>
+        </div>
+    </div>
 }
